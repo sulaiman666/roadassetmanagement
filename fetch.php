9b1efd3a8dd796e542552d1b45f267ecf1ee0b04
TheCrimson <sulaimaniman6@gmail.com>
2021-01-06 11:31:12 +0700

    Cleaning

--- fetch.php
+++ fetch.php
@@ -2,38 +2,47 @@
     include 'db.php';
     $desa = $_REQUEST["desa"];
     //Keseluruhan
-    //$fetch_sql = "SELECT * FROM public.clean WHERE kode_desa = $desa;";
-
-    $info_sql = "SELECT nama_desa, tahun FROM public.clean WHERE kode_desa = $desa LIMIT 1;";
+    //$fetch_sql = "SELECT * FROM public.gresik_rumija_d WHERE kode_desa = $desa;";
+    /*=========================================================Untuk Tabel Rumija============================================================*/ 
+    $info_sql = "SELECT nama_desa, tahun FROM public.gresik_rumija_d WHERE kode_desa = $desa LIMIT 1;";
     $info = pg_fetch_all(pg_query($dbcon, $info_sql));
     foreach ($info as $info) : 
         $tahun = $info['tahun']; 
         $nama_desa = $info['nama_desa'];
     endforeach;
 
-    $luas_sql = "SELECT SUM(luas) AS luas_total FROM public.clean WHERE kode_desa = $desa;";
+    $luas_sql = "SELECT SUM(luas) AS luas_total FROM public.gresik_rumija_d WHERE kode_desa = $desa;";
     $luas_result = pg_fetch_all(pg_query($dbcon, $luas_sql));
     foreach ($luas_result as $luas_result) : 
         $luas_total = $luas_result['luas_total']; 
     endforeach;
 
-    $sesuai_sql = "SELECT SUM(luas) AS luas_sesuai FROM public.clean WHERE kode_desa = $desa AND keterangan = 'SESUAI';";
+    $sesuai_sql = "SELECT SUM(luas) AS luas_sesuai FROM public.gresik_rumija_d WHERE kode_desa = $desa AND keterangan = 'SESUAI';";
     $sesuai_result = pg_fetch_all(pg_query($dbcon, $sesuai_sql));
     foreach ($sesuai_result as $sesuai_result) : 
         $sesuai_total = $sesuai_result['luas_sesuai']; 
     endforeach;
 
-    $estimasi_sql = "SELECT SUM(luas) AS luas_estimasi FROM public.clean WHERE kode_desa = $desa AND keterangan = 'ESTIMASI';";
+    $estimasi_sql = "SELECT SUM(luas) AS luas_estimasi FROM public.gresik_rumija_d WHERE kode_desa = $desa AND keterangan = 'ESTIMASI';";
     $estimasi_result = pg_fetch_all(pg_query($dbcon, $estimasi_sql));
     foreach ($estimasi_result as $estimasi_result) : 
         $estimasi_total = $estimasi_result['luas_estimasi']; 
     endforeach;
 
-    $okupasi_sql = "SELECT SUM(luas) AS luas_okupasi FROM public.clean WHERE kode_desa = $desa AND keterangan = 'OKUPASI';";
+    $okupasi_sql = "SELECT SUM(luas) AS luas_okupasi FROM public.gresik_rumija_d WHERE kode_desa = $desa AND keterangan = 'OKUPASI';";
     $okupasi_result = pg_fetch_all(pg_query($dbcon, $okupasi_sql));
     foreach ($okupasi_result as $okupasi_result) : 
         $okupasi_total = $okupasi_result['luas_okupasi']; 
     endforeach;
+
+    /*============================================================Untuk Tabel DT4============================================================*/
+    // /*-----------------------------------------------------------DT4_Point----------------------------------------------------------------*/
+    $dt4_point_sql = "SELECT layer, count(layer) AS total_unit FROM public.dt4_point WHERE kode_desa = $desa GROUP BY layer";
+    $dt4_point_result = pg_fetch_all(pg_query($dbcon, $dt4_point_sql));
+    
+    // /*-----------------------------------------------------------DT4_Line----------------------------------------------------------------*/
+    $dt4_line_sql = "SELECT layer, count(layer) AS total_unit FROM public.dt4_linestring WHERE kode_desa = $desa GROUP BY layer";
+    $dt4_line_result = pg_fetch_all(pg_query($dbcon, $dt4_line_sql));
     
 ?>
 <!DOCTYPE html>
@@ -64,7 +73,41 @@
                             <td><?php echo htmlspecialchars($estimasi_total); ?></td>
                             <td><?php echo htmlspecialchars($okupasi_total); ?></td>
                         </tr>
-                    
+                </tbody>
+            </table>
+            <h2>Data Teknik 4</h2>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Tahun</th>
+                        <th>Jenis Perlengkapan Jalan</th>
+                        <!-- <th>KI</th>
+                        <th>MID</th>
+                        <th>KA</th> -->
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($dt4_line_result as $dt4_line_result) :?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($tahun); ?></td>
+                            <td><?php echo htmlspecialchars($dt4_line_result['layer']); ?></td>
+                            <!-- <td>2</td>
+                            <td>-</td>
+                            <td>1</td> -->
+                            <td><?php echo htmlspecialchars($dt4_line_result['total_unit']); ?></td>
+                        </tr>
+                    <?php endforeach;?>
+                    <?php foreach($dt4_point_result as $dt4_point_result) :?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($tahun); ?></td>
+                            <td><?php echo htmlspecialchars($dt4_point_result['layer']); ?></td>
+                            <!-- <td>2</td>
+                            <td>-</td>
+                            <td>1</td> -->
+                            <td><?php echo htmlspecialchars($dt4_point_result['total_unit']); ?></td>
+                        </tr>
+                    <?php endforeach;?>
                 </tbody>
             </table>
         </div>
